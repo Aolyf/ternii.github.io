c650731018034ff1907c167fa586879ab293b1d8
ㅤㅤ <ccintj@gmail.com>
2017-02-10 08:32:57 -0500

    okay

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('img').addClass('animated rollIn');
+});
+$(document).ready(function(){
+    $("a").hover(function(){
+        $(this).css("background-color", "white");
+        });
+});
