f02e7cd009b30d077cce463c257a7ba080557626
calico <ccintj@gmail.com>
2017-07-10 18:10:38 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
-$(document).ready(function(){
-  $('img').addClass('animated rollIn');
-});
-$(document).ready(function(){
-    $("a").hover(function(){
-        $(this).css("background-color", "white");
-        });
-});
+function main(){
+  $('.container').hide();
+  $('.container').fadeIn(1000);
+  $('#ct').hide();
+  $('#bio').hide();
+  $('#music').hide();
+ $('#ct-btn').on('click', function(){
+    $('#ct').slideToggle(200);
+  });
+  $('#bio-btn').on('click', function(){
+    $('#bio').slideToggle(200);
+  });
+  $('#ms-btn').on('click', function(){
+    $('iframe').slideToggle(200);
+  });
+  
+}
+$(document).ready(main);
